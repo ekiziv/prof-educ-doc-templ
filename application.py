2f592a8eb84716632bc2dbc9d5f672fa6057a91d
Eleonora Kiziv <37271985+ekiziv@users.noreply.github.com>
2024-09-13 18:25:16 +0000

    adds support for categories in tractor cert docs

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import utils 
+import utils
 import picture
 
 import streamlit as st
@@ -15,6 +15,7 @@ from docx.oxml import register_element_cls
 
 NAME_KEY = 'student_name'
 CERTIFICATE_KEY = 'certificate_number'
+MACHINE_CATEGORY = 'machine_category'
 
 register_element_cls('wp:anchor', picture.CT_Anchor)
 
@@ -48,9 +49,10 @@ def choose_teacher(all_teachers):
                 st.warning("Введите ФИО преподавателя и место работы.")
 
     if selected_teacher:
-        return selected_teacher 
+        return selected_teacher
 
     return None
+
 
 def choose_profession(all_professions):
     """Handles profession selection and adding new professions,
@@ -68,7 +70,7 @@ def choose_profession(all_professions):
 
     # Checkbox to trigger new profession input
     add_new = st.checkbox("Добавить новую программу обучения?")
-    new_profession = None 
+    new_profession = None
 
     if add_new:  # Only show input if checkbox is checked
         new_profession = st.text_input("Введите название новой программы:")
@@ -76,28 +78,33 @@ def choose_profession(all_professions):
         if st.button("Добавить программу"):
             if new_profession:
                 code_int = -1
-                try: 
+                try:
                     code_int = int(new_code)
-                    all_professions[new_profession] = [code_int] 
-                except Exception as e: 
+                    all_professions[new_profession] = [code_int]
+                except Exception as e:
                     all_professions[new_profession] = [-1]
-                utils.save_data(all_professions, filename="data/professions.pickle")
+                utils.save_data(all_professions,
+                                filename="data/professions.pickle")
                 st.success(f"Программа '{new_profession}' добавлена!")
-                return f'{code_int} «{new_profession}»'  # Return the new profession if added
+                # Return the new profession if added
+                return f'{code_int} «{new_profession}»'
             else:
                 st.warning("Программа уже существует или не введена.")
-    if selected_item: 
+    if selected_item:
         selected_profession_code = all_professions[selected_item]
-        selected_profession_code_str = ', '.join(str(code) for code in selected_profession_code)
-        return f'{selected_profession_code_str} «{selected_item}»'  # Return selected_item otherwise
+        selected_profession_code_str = ', '.join(
+            str(code) for code in selected_profession_code)
+        # Return selected_item otherwise
+        return f'{selected_profession_code_str} «{selected_item}»'
     return None, None
-        
+
+
 def create_certificate(replacement_dict, students):
     if not students:
         return Document()
 
     all_paragraphs = []
-    for student in students[1:]: 
+    for student in students[1:]:
         local_dict = replacement_dict.copy()
         local_dict[NAME_KEY] = student[NAME_KEY]
         local_dict[CERTIFICATE_KEY] = student[CERTIFICATE_KEY]
@@ -125,19 +132,20 @@ def create_certificate(replacement_dict, students):
         # this ensure that the rows are not split between pages https://github.com/python-openxml/python-docx/issues/245
         trPr = row._tr.get_or_add_trPr()
         trPr.append(OxmlElement('w:cantSplit'))
-        
+
         target_cell = row.cells[0]
         source_cell = table.cell(0, 0)  # Use the first cell as a template
 
         # Copy cell properties from the template cell
-        utils.copy_cell_properties(source_cell, target_cell) 
+        utils.copy_cell_properties(source_cell, target_cell)
 
         # Add paragraphs and runs to the new cell, copying formatting
         for p_i, paragraph in enumerate(paragraphs):
             new_paragraph = target_cell.add_paragraph('')
             source_paragraph = source_cell.paragraphs[p_i]
-            if p_i == 0: 
-                picture.add_float_picture(new_paragraph, 'pictures/basic-cert-background.png', width=Inches(5.6), height=Inches(4.04), pos_x=Pt(0), pos_y=Pt(0))
+            if p_i == 0:
+                picture.add_float_picture(new_paragraph, 'pictures/basic-cert-background.png',
+                                          width=Inches(5.6), height=Inches(4.04), pos_x=Pt(0), pos_y=Pt(0))
 
             new_paragraph.alignment = source_paragraph.alignment
             new_paragraph.paragraph_format.left_indent = source_paragraph.paragraph_format.left_indent
@@ -145,6 +153,7 @@ def create_certificate(replacement_dict, students):
                 new_run = new_paragraph.add_run(target_run.text)
                 utils.preserve_formatting(new_run, source_run)
     return final_doc
+
 
 def create_tractor_certificate(replacement_dict, students):
     if not students:
@@ -155,6 +164,8 @@ def create_tractor_certificate(replacement_dict, students):
         local_dict = replacement_dict.copy()
         local_dict[NAME_KEY] = student[NAME_KEY]
         local_dict[CERTIFICATE_KEY] = student[CERTIFICATE_KEY]
+        if MACHINE_CATEGORY in student:
+            local_dict[MACHINE_CATEGORY] = student[MACHINE_CATEGORY]
 
         doc = DocxTemplate('templates/Свидетельство_трактор.docx')
         doc.render(local_dict)  # Assuming you have a 'render' method defined
@@ -167,6 +178,8 @@ def create_tractor_certificate(replacement_dict, students):
     local_dict = replacement_dict.copy()
     local_dict[NAME_KEY] = students[0][NAME_KEY]
     local_dict[CERTIFICATE_KEY] = students[0][CERTIFICATE_KEY]
+    if MACHINE_CATEGORY in students[0]:
+        local_dict[MACHINE_CATEGORY] = students[0][MACHINE_CATEGORY]
     final_doc.render(local_dict)
 
     utils.set_default_font(final_doc)
@@ -178,31 +191,32 @@ def create_tractor_certificate(replacement_dict, students):
         # this ensure that the rows are not split between pages https://github.com/python-openxml/python-docx/issues/245
         trPr = row._tr.get_or_add_trPr()
         trPr.append(OxmlElement('w:cantSplit'))
-        
+
         left_cell = row.cells[0]
         right_cell = row.cells[1]
-        
-        source_right_cell = table.cell(0, 1) 
+
+        source_right_cell = table.cell(0, 1)
         source_left_cell = table.cell(0, 0)
 
         # Copy cell properties from the template cell
-        utils.copy_cell_properties(source_left_cell, left_cell) 
-        utils.copy_cell_properties(source_right_cell, right_cell) 
+        utils.copy_cell_properties(source_left_cell, left_cell)
+        utils.copy_cell_properties(source_right_cell, right_cell)
 
         # Populate left cell
         for p_i, paragraph in enumerate(paragraphs):
             new_paragraph = left_cell.add_paragraph()
             if p_i == 0:
-                picture.add_float_picture(new_paragraph, 'pictures/tractor-cert.png', width=Inches(8.03), height=Inches(5.58), pos_x=Pt(0), pos_y=Pt(0))
+                picture.add_float_picture(new_paragraph, 'pictures/tractor-cert.png', width=Inches(
+                    8.03), height=Inches(5.58), pos_x=Pt(0), pos_y=Pt(0))
             source_paragraph = source_left_cell.paragraphs[p_i]
             new_paragraph.alignment = source_paragraph.alignment
             new_paragraph.paragraph_format.left_indent = source_paragraph.paragraph_format.left_indent
-            
+
             for target_run, source_run in zip(paragraph.runs, source_paragraph.runs):
                 new_run = new_paragraph.add_run(target_run.text)
                 new_run = utils.preserve_formatting(new_run, source_run)
-        
-        for p in source_right_cell.paragraphs: 
+
+        for p in source_right_cell.paragraphs:
             new_paragraph = right_cell.add_paragraph('')
             new_paragraph.alignment = p.alignment
             for source_run in p.runs:
@@ -215,9 +229,9 @@ def create_beginning_document(beginning_dict, students):
     """Creates a Word document with the provided information."""
 
     doc = DocxTemplate('templates/Приказ о начале.docx')
-    doc.render(beginning_dict) 
+    doc.render(beginning_dict)
     utils.set_default_font(doc)
-    table = doc.tables[0] 
+    table = doc.tables[0]
 
     for index, student in enumerate(students):
         # Create a new row
@@ -228,16 +242,17 @@ def create_beginning_document(beginning_dict, students):
         new_row.cells[1].text = student[NAME_KEY]
         new_row.cells[2].text = beginning_dict['student_company']
 
-        # Add more cells for other data (profession, date, etc.) if needed 
+        # Add more cells for other data (profession, date, etc.) if needed
     return doc
+
 
 def create_end_doc(replacement_dict, students):
     """Creates a Word document with the provided information."""
 
     doc = DocxTemplate('templates/Приказ о выпуске.docx')
-    doc.render(replacement_dict) 
+    doc.render(replacement_dict)
     utils.set_default_font(doc)
-    table = doc.tables[0] 
+    table = doc.tables[0]
 
     for index, student in enumerate(students):
         # Create a new row
@@ -250,14 +265,15 @@ def create_end_doc(replacement_dict, students):
         new_row.cells[3].text = str(student[CERTIFICATE_KEY])
 
     return doc
+
 
 def create_protocol_doc(replacement_dict, students):
     """Creates a Word document with the provided information."""
 
     doc = DocxTemplate('templates/Протокол.docx')
-    doc.render(replacement_dict) 
+    doc.render(replacement_dict)
     utils.set_default_font(doc)
-    table = doc.tables[0] 
+    table = doc.tables[0]
 
     for index, student in enumerate(students):
         # Create a new row
@@ -270,6 +286,7 @@ def create_protocol_doc(replacement_dict, students):
         new_row.cells[3].text = str(student[CERTIFICATE_KEY])
 
     return doc
+
 
 st.title("Профессиональное обучение")
 
@@ -277,8 +294,8 @@ st.title("Профессиональное обучение")
 available_professions = utils.load_from_pickle('data/professions.pickle')
 student_profession = choose_profession(available_professions)
 
-beginning_date = st.date_input('дата начала',value=datetime.date(2025, 1, 1))
-end_date = st.date_input('дата окончания',value=datetime.date(2025, 1, 1))
+beginning_date = st.date_input('дата начала', value=datetime.date(2025, 1, 1))
+end_date = st.date_input('дата окончания', value=datetime.date(2025, 1, 1))
 
 beginning_number = st.number_input("номер приказа о начале", 808)
 end_number = st.number_input("номер приказа об окончании", 808)
@@ -286,33 +303,36 @@ end_number = st.number_input("номер приказа об окончании"
 # this should be replaced by a scroll through
 teacher_name = choose_teacher(utils.load_from_pickle('data/teachers.pickle'))
 
-company = st.text_input("Предприятие", 'заявление', placeholder="Наименование предприятия или 'заявление'")
-student_names = st.text_area("Введите имена студентов, по одному на строку").split("\n")
+company = st.text_input("Предприятие", 'заявление',
+                        placeholder="Наименование предприятия или 'заявление'")
+student_names = st.text_area(
+    "Введите имена студентов, по одному на строку").split("\n")
 student_data = []
 for line in student_names:
     if line:
-        filtered_items = [item for item in line.split('\t') if item] 
-        certificate_number, _, _, name  = filtered_items  # Split each line by tab
+        filtered_items = [item for item in line.split('\t') if item]
+        certificate_number, _, _, name, *category = filtered_items  # Split each line by tab
         student_data.append({
             NAME_KEY: name,
             CERTIFICATE_KEY: int(float(certificate_number)),
+            MACHINE_CATEGORY: category[0] if category else None
         })
 num_students = len(student_data)
 
 formatted_beginning_date = utils.format_date(beginning_date)
 formatted_end_date = utils.format_date(end_date)
 replacement_dict = {
-        'beginning_date': formatted_beginning_date, 
-        'beginning_number': beginning_number, 
-        'end_date': formatted_end_date, 
-        'end_number': end_number,
-        'student_profession': student_profession, 
-        'student_company': company, 
-        'teacher_name': teacher_name, 
-        'num_students': num_students,
-        'class': '3', 
-        'year': beginning_date.year, 
-    }
+    'beginning_date': formatted_beginning_date,
+    'beginning_number': beginning_number,
+    'end_date': formatted_end_date,
+    'end_number': end_number,
+    'student_profession': student_profession,
+    'student_company': company,
+    'teacher_name': teacher_name,
+    'num_students': num_students,
+    'class': '3',
+    'year': beginning_date.year,
+}
 beginning_doc = create_beginning_document(replacement_dict, student_data)
 end_doc = create_end_doc(replacement_dict, student_data)
 protocol = create_protocol_doc(replacement_dict, student_data)
@@ -322,24 +342,24 @@ tractor_cert_doc = create_tractor_certificate(replacement_dict, student_data)
 show_documents = st.button("Сгенерировать документы")
 
 if show_documents:
-    if not student_profession: 
+    if not student_profession:
         st.warning('Укажите профессию')
-    if not teacher_name: 
+    if not teacher_name:
         st.warning('Укажите преподователя')
-    if not beginning_date: 
+    if not beginning_date:
         st.warning('Укажите дату начала')
-    if not end_date: 
+    if not end_date:
         st.warning('Укажите дату окончания')
-    if not beginning_number: 
+    if not beginning_number:
         st.warning('Укажите номер приказа о начале')
-    if not end_number: 
+    if not end_number:
         st.warning('Укажите номер приказа о выпуске')
-    if num_students == 0: 
+    if num_students == 0:
         st.warning('Укажите обучающихся')
 
-
-    if student_profession and teacher_name and beginning_date and end_date and beginning_number and end_number: 
-        document_tabs = st.tabs(["Приказ о начале", "Приказ об окончании", "Протокол", "Свидетельство", "Свидетельство тракторов"])
+    if student_profession and teacher_name and beginning_date and end_date and beginning_number and end_number:
+        document_tabs = st.tabs(["Приказ о начале", "Приказ об окончании",
+                                "Протокол", "Свидетельство", "Свидетельство тракторов"])
         with document_tabs[0]:  # Приказ о начале
             utils.display_docx_content(beginning_doc)
 
@@ -365,10 +385,10 @@ with zipfile.ZipFile(zip_buffer, 'w') as zipf:
     # Add the end document
     with zipf.open('Приказ о выпуске.docx', 'w') as f:
         end_doc.save(f)
-    
+
     with zipf.open('Протокол.docx', 'w') as f:
         protocol.save(f)
-    
+
     with zipf.open('Свидетельство.docx', 'w') as f:
         certificate_docs.save(f)
 
@@ -384,4 +404,3 @@ st.download_button(
     file_name=f'{company}-{student_profession}-{end_date}.zip',
     mime='application/zip'
 )
-    
